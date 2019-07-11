5e25a9634c6284a92fefe71c2c8510a1707f4043
saki15048 <15048@hakodate.kosen-ac.jp>
2019-07-11 09:39:38 +0900

    Bがキャラ移動処理を追加

--- gftest/Asan.java
+++ gftest/Asan.java
@@ -14,6 +14,9 @@ public class Asan extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+    }// Add your action code here.
     }    
 }
