f72beeb9dfc041367ec6f6bfd1aca29cbce3f7e3
佐藤 歩夢 <15083@kauth.local>
2019-07-11 09:25:01 +0900

    Aがキャラ移動処理を追加

--- gftest/Asan.java
+++ gftest/Asan.java
@@ -14,6 +14,10 @@ public class Asan extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+        setRotation(270);
+        move(1);
+}
+// Add your action code here.
     }    
 }
